6cc1928bfc59c1afb82f3a855ac4f83bc0a5d28b
Takdanai <jetak55.tak@gmail.com>
2024-10-06 01:34:57 +0700

    add : cors

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI,HTTPException
 from app.routers import place, province, provinceDashboard, province_place
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -9,3 +9,24 @@ app.include_router(province.router)
 app.include_router(provinceDashboard.router)
 app.include_router(province_place.router)
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000", 
+    "https://geogle.kitton.dev/", 
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def main():
+    return {"message": "Hello World"}
