aef8536b7fc0370f1e5ddfa56feecd780554c994
Kittipod Lambangchang <tonkittipod@gmail.com>
2024-10-06 03:28:17 +0700

    fix: fix bug

--- app/services/province_place_service.py
+++ app/services/province_place_service.py
@@ -15,14 +15,14 @@ import time
 
 def get_body(url):
     chrome_options = Options()
-    chrome_optionsadd_argument("--headless")
-    chrome_optionsadd_argument("--no-sandbox")
-    chrome_optionsadd_argument("--disable-dev-shm-usage")
-    chrome_optionsadd_argument("--disable-browser-side-navigation")
-    chrome_optionsadd_argument("--disable-gpu")
-    chrome_optionsadd_argument("--window-size=1920,1080")
-    chrome_optionsadd_argument("start-maximized")
-    chrome_optionsadd_argument("enable-automation")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-browser-side-navigation")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("start-maximized")
+    chrome_options.add_argument("enable-automation")
 
     
 
