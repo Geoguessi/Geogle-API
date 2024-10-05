689861033d422e2a3c7ac8fe31b346bba5b8a08b
Kittipod Lambangchang <tonkittipod@gmail.com>
2024-10-06 02:41:06 +0700

    chore: update code

--- app/services/province_place_service.py
+++ app/services/province_place_service.py
@@ -16,8 +16,11 @@ import time
 def get_body(url):
     chrome_options = Options()
     chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--headless')
+    # chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-dev-shm-usage')  
+    chrome_options.add_argument('--remote-debugging-port=9222')  
+
     
 
     driver = None
