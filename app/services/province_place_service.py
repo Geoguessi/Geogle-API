a007a80eb3b0f434f2a97a7a3d4f339e8549a902
Kittipod Lambangchang <tonkittipod@gmail.com>
2024-10-06 02:37:41 +0700

    chore: update code

--- app/services/province_place_service.py
+++ app/services/province_place_service.py
@@ -16,7 +16,6 @@ import time
 def get_body(url):
     chrome_options = Options()
     chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--window-size=1420,1080')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
     
